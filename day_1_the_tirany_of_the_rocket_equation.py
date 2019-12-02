1123a59bd86a19a43c9da1ad8908854327c902dc
luucamay <lupemaydana@gmail.com>
2019-12-01 22:44:16 -0400

    Part 1 solution day 1

--- day_1_the_tirany_of_the_rocket_equation.py
+++ day_1_the_tirany_of_the_rocket_equation.py
@@ -1,21 +1,15 @@
 '''
 The tirany of the rocket equation - Advent of Code
 '''
+# Part 1
 def fuel_required_by_module(mass):
     return int(mass/float(3)) - 2
 
-def total_fuel_required(arr_mass):
-    total = 0
-    for mass in arr_mass:
-        total += fuel_required_by_module(mass)
-    return total
-
-arr_mass = []
+total = 0
 with open("input.txt") as file:
     for line in file:
-        t = int(line)
-        print t
-        arr_mass.append(t)
-print total_fuel_required(arr_mass)
+        mass = int(line)
+        total += fuel_required_by_module(mass)
+print total
 
 
