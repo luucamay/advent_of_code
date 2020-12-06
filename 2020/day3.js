e743d3f163fccf8fbb7c1ff60e68f639416fd822
Lupe <lupemaydana@gmail.com>
2020-12-06 19:54:05 -0400

    feat: Solution to day 3 part 2 brute force

--- 2020/day3.js
+++ 2020/day3.js
@@ -11,12 +11,12 @@
 */
 
 // Count the trees found while following down in the toboggan
-const countTrees = (map) => {
+const countTrees = (map, positionToAdd) => {
   let totalTrees = 0;
   const heightSize = map.length;
   const widthSize = map[0].length;
   let nextStep = 0;
-  const positionToAdd = [1, 3];
+  // const positionToAdd = [1, 3];
   // while we do not arrive to the bottom keep running!
   let currRow = 0;
   let currCol = 0;
@@ -49,7 +49,14 @@ fsPromises.readFile('input.txt', 'utf-8')
   .then((result) => {
     newMap = getMap(result);
     // Part 1
-    console.log(countTrees(newMap));
+    console.log(countTrees(newMap, [1, 3]));
+    const answers = []
+    answers[0] = countTrees(newMap, [1,1]);
+    answers[1] = countTrees(newMap, [1,3]);
+    answers[2] = countTrees(newMap, [1,5]);
+    answers[3] = countTrees(newMap, [1,7]);
+    answers[4] = countTrees(newMap, [2,1]);
+    console.log(answers[0]*answers[1]*answers[2]*answers[3]*answers[4]);
     // Part 2
     // console.log(countValidPasswords(arrayStrings, isValidPassword2));
   })
