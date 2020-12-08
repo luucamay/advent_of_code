b09eb94ee7660cddc91ed2e637e3c3c378506ecf
Lupe <lupemaydana@gmail.com>
2020-12-07 21:19:23 -0400

    docs: Add description

---
+++ 2020/day4.js
@@ -0,0 +1,11 @@
+/*
+* Main goal: Get 50 statusbar
+* Today goal: Count valid passwords by improving the validator system
+* How:
+** read the input
+** transform input into array of objects
+** create a function to transform input(array of objects) into the total valid passports
+** create a function to validate a passport!
+*/
+
+
