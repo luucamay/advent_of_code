86bba5d588811f8620de850ce34c032508f57cdb
Lupe <lupemaydana@gmail.com>
2020-12-07 21:53:31 -0400

    feat: solution to day 4 part 1

--- 2020/day4.js
+++ 2020/day4.js
@@ -8,4 +8,68 @@
 ** create a function to validate a passport!
 */
 
+const isValidPassport = (passport) => {
+  // check fields
+  const fields = [
+    'byr',
+    'iyr',
+    'eyr',
+    'hgt',
+    'hcl',
+    'ecl',
+    'pid',
+    'cid'
+  ];
+  if (fields[0] in passport &&
+    fields[1] in passport &&
+    fields[2] in passport &&
+    fields[3] in passport &&
+    fields[4] in passport &&
+    fields[5] in passport &&
+    fields[6] in passport)
+    return true;
+  return false;
+}
 
+const countValidPassports = (passportList) => {
+  let totalValid = 0;
+  for (passportObj of passportList) {
+    if (isValidPassport(passportObj))
+      totalValid++;
+  }
+  return totalValid;
+}
+
+const transformInput = (inputText) => {
+  const passportTextList = inputText.split('\r\n\r\n');
+  const passportObjList = [];
+  for (passportText of passportTextList) {
+    const linesPassport = passportText.split('\r\n');
+    const passport = {}
+    for (line of linesPassport) {
+      const propertiesStr = line.split(' ');
+      propertiesStr.forEach((propertyStr) => {
+        const [key, value] = propertyStr.split(':');
+        passport[key] = value;
+      })
+    }
+    passportObjList.push(passport);
+  }
+  return passportObjList;
+}
+
+
+const fs = require('fs');
+const fsPromises = fs.promises;
+
+fsPromises.readFile('input.txt', 'utf-8')
+  .then((result) => {
+    const passportArray = transformInput(result);
+    const total = countValidPassports(passportArray);
+    console.log(total);
+    // Part 1
+    // Part 2
+  })
+  .catch((error) => {
+    console.log(error);
+  });
