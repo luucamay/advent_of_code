7b55d0e58da85440ffece07d6655abee092631e2
Lupe <lupemaydana@gmail.com>
2020-12-08 21:08:42 -0400

    feat: Solution to day 6 part 2

--- 2020/day6.js
+++ 2020/day6.js
@@ -25,16 +25,46 @@ const totalQuestionsAnswered = (answerList) => {
   return total;
 }
 
+// Part 2
+const countQuestionsAnsweredByEveryone = (answersOfGroup) => {
+  // Each line has the answer of each person
+  const answerLines = answersOfGroup.split('\r\n');
+  const firstLine = answerLines[0];
+  let countAnswers = 0;
+  for (const answer of firstLine) {
+    let everyoneAnswered = true;
+    for (let i = 1; i < answerLines.length; i++) {
+      const line = answerLines[i];
+      if (!(line.includes(answer))) {
+        everyoneAnswered = false;
+        continue;
+      }
+    }
+    if (everyoneAnswered)
+      countAnswers++;
+  }
+  return countAnswers;
+}
+
+const totalQuestionsAnsweredByEveryone = (answerList) => {
+  let total = 0;
+  for (const groupAnswers of answerList) {
+    const count = countQuestionsAnsweredByEveryone(groupAnswers);
+    total += count;
+  }
+  return total;
+}
+
 const fs = require('fs');
 const fsPromises = fs.promises;
 
 fsPromises.readFile('input.txt', 'utf-8')
   .then((result) => {
-    const answers = transformInput(result);
+    const groupAnswers = transformInput(result);
     // Part 1
-    console.log(totalQuestionsAnswered(answers));
-
+    console.log(totalQuestionsAnswered(groupAnswers));
     // Part 2
+    console.log(totalQuestionsAnsweredByEveryone(groupAnswers));
   })
   .catch((error) => {
     console.log(error);
