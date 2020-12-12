756ac9e12e9eab52d3e4c8e9ec9e99e30117f943
Lupe <lupemaydana@gmail.com>
2020-12-11 22:20:34 -0400

    feat: Solution to day 10 part 2

--- 2020/day10.js
+++ 2020/day10.js
@@ -7,6 +7,17 @@
 ** start counter of 1's and 3's differences
 ** traverse the array and find differences
 ** multiply counters
+* How part 2: How many different ways you can arrange your adapters?
+** Traverse the list of adapters from right to left
+** Find the most right to remove (diff of prev and next < 3)
+** Save in a stack of waysToArrange?
+** Find next most right to remove, add all the many ways already in the stack of waysToArrange
+** still do not know how to find the total count (sum of subtotals)
+* Part 2 was hard so here I will leave some links that helped me to understand better!
+*** Additional short test cases: https://www.reddit.com/r/adventofcode/comments/kapbjb/year_2020_day_10_part_2_additional_examples_for/
+*** A meme: https://www.reddit.com/r/adventofcode/comments/kamfm0/2020_day_10_always_overshadowed_by_his_little/
+*** An easy python solution: https://www.reddit.com/r/adventofcode/comments/ka8z8x/2020_day_10_solutions/gfdfthy?utm_source=share&utm_medium=web2x&context=3
+
 */
 const transformInput = (input) => input.split('\r\n').map((num) => parseInt(num));
 
@@ -29,6 +40,27 @@ const numberJoltDifferences = (rateAdapterList) => {
   return diffs[0] * diffs[2];
 }
 
+const numberWaysToArrange = (adpaters) => {
+  //adpaters.push(0);
+  adpaters.sort((a, b) => a - b);
+  const size = adpaters.length;
+  const last = adpaters[size - 1];
+  const dp = new Array(last + 1).fill(0);
+  dp[0] = 1;
+  for (const r of adpaters) {
+    //console.log(dp);
+    if (r >= 3)
+      dp[r] = dp[r - 1] + dp[r - 2] + dp[r - 3];
+    if (r === 2)
+      dp[r] = dp[r - 1] + dp[r - 2];
+    if (r === 1)
+      dp[r] = dp[r - 1];
+    if (r === last)
+      return dp[r];
+  }
+  return 0;
+}
+
 const fs = require('fs');
 const fsPromises = fs.promises;
 
@@ -39,6 +71,8 @@ fsPromises.readFile('input.txt', 'utf-8')
     const multipliedDiffs = numberJoltDifferences(numberList);
     console.log({ multipliedDiffs });
     // Part 2
+    const waysArranged = numberWaysToArrange(numberList);
+    console.log({ waysArranged });
   })
   .catch((error) => {
     console.log(error);
