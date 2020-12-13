f778dcfa75ce34023d2f0c1a70cec18e543a37d8
Lupe <lupemaydana@gmail.com>
2020-12-13 18:25:28 -0400

    feat: Solution to day 12 part 2

--- 2020/day12.js
+++ 2020/day12.js
@@ -39,20 +39,9 @@ const getCoords = (instructions) => {
       currDirections = changeDirection(currDirections, instruction, value);
     } else {
       const newDir = instruction === 'F' ? currDirections[0] : instruction;
-      switch (newDir) {
-        case 'N':
-          position[0] += value;
-          break;
-        case 'S':
-          position[0] -= value;
-          break;
-        case 'E':
-          position[1] += value;
-          break;
-        default:
-          position[1] -= value;
-          break;
-      }
+      const newPos = updatePostion(newDir, value);
+      position[0] += newPos[0];
+      position[1] += newPos[1];
     }
   }
   // console.log(position);
@@ -60,6 +49,68 @@ const getCoords = (instructions) => {
 }
 
 const transformInput = (input) => input.split('\r\n');
+
+const updatePostion = (direction, steps) => {
+  const newPosition = [0, 0];
+  switch (direction) {
+    case 'N':
+      newPosition[0] += steps;
+      break;
+    case 'S':
+      newPosition[0] -= steps;
+      break;
+    case 'E':
+      newPosition[1] += steps;
+      break;
+    default:
+      newPosition[1] -= steps;
+      break;
+  }
+  return newPosition;
+}
+
+// Part 2
+
+const changeDirectionWaypoint = (currDirections, position, instruction, value) => {
+  const numTimes = value / 90;
+  let finalPosition = [0, 0];
+  const rotateFunction = instruction === 'R' ? rotate : rotateInverse;
+  for (let t = 0; t < numTimes; t++) {
+    currDirections = rotateFunction(currDirections);
+    // change first coordinate: define if it goes to row or column
+    let newPostion1 = updatePostion(currDirections[0], position[0]);
+    let newPosition2 = updatePostion(currDirections[1], position[1]);
+    finalPosition[0] = newPostion1[0] + newPosition2[0];
+    finalPosition[1] = newPostion1[1] + newPosition2[1];
+  }
+  return finalPosition;
+}
+
+const getCoordsWaypointShip = (instructions) => {
+  let currDirections = 'NESW';
+  // north/south at row, east/west at column
+  let relPosWaypoint = [1, 10];
+  let posShip = [0, 0];
+  for (const line of instructions) {
+    const instruction = line[0];
+    const value = parseInt(line.slice(1));
+
+    if (instruction === 'L' || instruction === 'R') {
+      relPosWaypoint = changeDirectionWaypoint(currDirections, relPosWaypoint, instruction, value);
+    } else {
+      // updatePostions
+      if (instruction !== 'F') {
+        const newPos = updatePostion(instruction, value);
+        relPosWaypoint[0] += newPos[0];
+        relPosWaypoint[1] += newPos[1];
+      } else {
+        posShip[0] += relPosWaypoint[0] * value;
+        posShip[1] += relPosWaypoint[1] * value;
+      }
+    }
+  }
+  return Math.abs(posShip[0]) + Math.abs(posShip[1]);
+}
 
 const fs = require('fs');
 const fsPromises = fs.promises;
@@ -71,6 +122,8 @@ fsPromises.readFile('input.txt', 'utf-8')
     const sum = getCoords(instructions);
     console.log({ sum });
     // Part 2
+    const sum2 = getCoordsWaypointShip(instructions);
+    console.log({ sum2 });
   })
   .catch((error) => {
     console.log(error);
