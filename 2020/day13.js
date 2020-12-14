af958b8c38e14cb5954ab16d1c02460f6bb2f8ba
Lupe <lupemaydana@gmail.com>
2020-12-13 21:49:59 -0400

    feat: Solution to day 13 part 2

--- 2020/day13.js
+++ 2020/day13.js
@@ -5,20 +5,23 @@
 ** split input by line and by ' ,'
 ** get Closest Divisible Integer that is greater of your time (https://stackoverflow.com/questions/29453717/closest-divisible-integer)
 ** find the min number of those numbers
+* How part 2:
+** hint 1: Chinese Remainder Theorem
+** hint 2: Python implementation some explanation (https://www.reddit.com/r/adventofcode/comments/kc4njx/2020_day_13_solutions/gfr2uh6?utm_source=share&utm_medium=web2x&context=3)
 */
 
 const closestDivisibleInteger = (a, b) => (a + b) - (a % b);
 
 const findEarliestBus = (timestamp, idBuses) => {
-  const arriveCloseTimes = idBuses.map((busId) => {
-    const time = closestDivisibleInteger(timestamp, busId);
-    return { busId, time };
+  const arriveCloseTimes = idBuses.map((id) => {
+    const time = closestDivisibleInteger(timestamp, id);
+    return { id, time };
   });
   const reducer = (min, item) => item.time < min ? item.time : min;
   const closestTime = arriveCloseTimes.reduce(reducer, arriveCloseTimes[0].time);
   const bus = arriveCloseTimes.find(ele => ele.time === closestTime);
   // calculate final result
-  const result = (bus.time - timestamp) * bus.busId;
+  const result = (bus.time - timestamp) * bus.id;
   return result;
 }
 
@@ -31,6 +34,35 @@ const transformInput = (input) => {
   return [yourtime, buses];
 }
 
+const getPairList = (input) => {
+  const [, list] = input.split('\r\n');
+  const buses = list.split(',');
+  const pairs = [];
+  for (let i = 0; i < buses.length; i++) {
+    if (buses[i] !== 'x') {
+      const pair = [parseInt(buses[i]), i];
+      pairs.push(pair);
+    }
+  }
+  const sortedBuses = pairs.sort(function (a, b) {
+    return b[0] - a[0];
+  });
+  return sortedBuses;
+}
+
+const getTimestamp = (buses) => {
+  let t = 0;
+  let step = 1;
+  for (const input of buses) {
+    let bus = input[0];
+    let offset = input[1];
+    while ((t + offset) % bus !== 0)
+      t += step;
+    step *= bus;
+  }
+  return t;
+}
+
 const fs = require('fs');
 const fsPromises = fs.promises;
 
@@ -41,6 +73,9 @@ fsPromises.readFile('input.txt', 'utf-8')
     const answer = findEarliestBus(timestamp, buses);
     console.log({ answer });
     // Part 2
+    const busPairs = getPairList(result);
+    const answer2 = getTimestamp(busPairs);
+    console.log({ answer2 });
   })
   .catch((error) => {
     console.log(error);
