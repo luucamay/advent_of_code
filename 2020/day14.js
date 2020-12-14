dd7bb52cae07c4dffd8057378729cbf2cbcbe912
Lupe <lupemaydana@gmail.com>
2020-12-14 17:38:54 -0400

    chore: Cleaning code

--- 2020/day14.js
+++ 2020/day14.js
@@ -3,15 +3,11 @@
 * Today goal: Decode the docked data
 * How part 1: Get the sum of all values written in memory
 ** get the masks and the writes to memory
-** create the list of pairs with bit and bitIndex for mask
-** replace 1's and 0's from the mask with clearBit and setBit
-** start at the end of the list to only write if there is nothing written in memory
 ** sum all the numbers writenn in memory
 * Consider:
-** the memory is always a 36 bits size (2^35)
-** set and unset the bits
-** get list of pairs (0, bitIndex) or (1, bitIndex)
+** the memory is always a 36 bits size (2^35) Use BigInt please!!!
 * Resource about bits => https://realpython.com/python-bitwise-operators/#bitmasks
+* Using BigInt -> https://dev.to/aumayeung/using-javascript-bigint-to-represent-large-numbers-2icf
 */
 
 const transformInput = (input) => {
@@ -25,7 +21,6 @@ const sumValsMemory = (memory) => {
   for (const val in memory) {
     sum += memory[val];
   }
-  console.log({ sum });
   return sum;
 }
 
@@ -36,18 +31,14 @@ const getSumAllValues = (keyVals) => {
     let [key, value] = line;
     if (key === 'mask') {
       mask = value;
-      console.log({ mask });
     } else {
       const address = key.slice(4, key.length - 1);
       value = BigInt(parseInt(value));
       const changeToCeros = BigInt(parseInt(mask.replace(/1/gi, '0').replace(/X/gi, '1'), 2));
       const changeToOnes = BigInt(parseInt(mask.replace(/X/gi, '0'), 2));
-      // console.log({ value, changeToCeros, changeToOnes });
       memory[address] = value & changeToCeros | changeToOnes;
-      console.log('new value: ',memory[address]);
     }
   }
-  //console.log(memory);
   return sumValsMemory(memory);
 }
 
@@ -57,9 +48,11 @@ const fsPromises = fs.promises;
 fsPromises.readFile('input.txt', 'utf-8')
   .then((result) => {
     const input = transformInput(result);
-    getSumAllValues(input);
     // Part 1
+    const sum = getSumAllValues(input);
+    console.log({ sum });
     // Part 2
+
   })
   .catch((error) => {
     console.log(error);
