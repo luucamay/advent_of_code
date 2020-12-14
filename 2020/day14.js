26c863497d5c02688e1df59429d051f38e470feb
Lupe <lupemaydana@gmail.com>
2020-12-14 18:44:05 -0400

    feat: Solution to day 14 part 2

--- 2020/day14.js
+++ 2020/day14.js
@@ -7,6 +7,7 @@
 * Consider:
 ** the memory is always a 36 bits size (2^35) Use BigInt please!!!
 * Resource about bits => https://realpython.com/python-bitwise-operators/#bitmasks
+* Big Hint and easy to read python solution: https://www.reddit.com/r/adventofcode/comments/kcr1ct/2020_day_14_solutions/gfshjtl?utm_source=share&utm_medium=web2x&context=3
 * Using BigInt -> https://dev.to/aumayeung/using-javascript-bigint-to-represent-large-numbers-2icf
 */
 
@@ -21,38 +22,53 @@ const sumValsMemory = (memory) => {
   for (const val in memory) {
     sum += memory[val];
   }
+  console.log({ sum });
   return sum;
+}
+
+const writeMemory = (memory, mask, address, value) => {
+  if (mask.includes('X')) {
+    let i = mask.indexOf('X');
+    writeMemory(memory, mask.slice(0, i) + '0' + mask.slice(i + 1), address, value);
+    writeMemory(memory, mask.slice(0, i) + '1' + mask.slice(i + 1), address, value);
+  } else {
+    const newAddress = BigInt(parseInt(mask, 2)) | address;
+    memory[newAddress] = value;
+  }
 }
 
 const getSumAllValues = (keyVals) => {
   const memory = {};
+  const memory2 = {}
   let mask;
   for (const line of keyVals) {
     let [key, value] = line;
     if (key === 'mask') {
       mask = value;
     } else {
-      const address = key.slice(4, key.length - 1);
-      value = BigInt(parseInt(value));
+      let address = BigInt(key.slice(4, key.length - 1));
+      value = BigInt(value);
       const changeToCeros = BigInt(parseInt(mask.replace(/1/gi, '0').replace(/X/gi, '1'), 2));
       const changeToOnes = BigInt(parseInt(mask.replace(/X/gi, '0'), 2));
       memory[address] = value & changeToCeros | changeToOnes;
+      //  part 2
+      address = address & BigInt(parseInt(mask.replace(/0/gi, '1').replace(/X/gi, '0'), 2));
+      writeMemory(memory2, mask, address, value);
     }
   }
-  return sumValsMemory(memory);
+  sumValsMemory(memory);
+  sumValsMemory(memory2);
 }
 
+const { memory } = require('console');
 const fs = require('fs');
+const { parse } = require('path');
 const fsPromises = fs.promises;
 
 fsPromises.readFile('input.txt', 'utf-8')
   .then((result) => {
     const input = transformInput(result);
-    // Part 1
-    const sum = getSumAllValues(input);
-    console.log({ sum });
-    // Part 2
-
+    getSumAllValues(input);
   })
   .catch((error) => {
     console.log(error);
