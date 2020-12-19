de1e9f0c550107d426376a8de2f1c265aca15b26
Lupe <lupemaydana@gmail.com>
2020-12-18 22:43:55 -0400

    feat: Solution to day 15 part 2

--- 2020/day15.js
+++ 2020/day15.js
@@ -8,26 +8,34 @@
 ** if it is the first time (key not in object), save current postion
 ** else calculate difference position last number and value in object
 ** change position number said in the object
+** really good advice (maps instead of objects): https://www.reddit.com/r/adventofcode/comments/kekmfh/2020_day_15_javascript_what_a_difference_the/?utm_source=share&utm_medium=web2x&context=3
 */
 
 const get2020NumSpoken = (numbers) => {
   let index = 0;
   const size = numbers.length;
-  const dictionary = {};
+  const dictionary = new Map();
   while (index < size - 1) {
     const key = numbers[index];
-    dictionary[key] = index;
+    dictionary.set(key, index);
     index += 1;
   }
-  while (index < 2020) {
+  let t0 = new Date();
+  while (index < 30000000) {
+
+    let t1 = new Date();
+    if (t1 - t0 >= 10000) {
+      console.log({ t1 });
+      t0 = t1;
+    }
     const lastNumber = numbers[index];
-    if (!dictionary.hasOwnProperty(lastNumber)) {
+    if (!dictionary.has(lastNumber)) {
       numbers.push(0);
     } else {
-      const timesApart = index - dictionary[lastNumber];
+      const timesApart = index - dictionary.get(lastNumber);
       numbers.push(timesApart)
     }
-    dictionary[lastNumber] = index;
+    dictionary.set(lastNumber, index);
     index += 1;
   }
   console.log('Final Number:', numbers[index - 1]);
