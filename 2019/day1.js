3b739e0c81c89ce9d893a76deae849d81ba0bbc4
Lupe CaMay <lupemaydana@gmail.com>
2021-06-27 11:10:31 -0700

    Update day1.js

--- 2019/day1.js
+++ 2019/day1.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let input = fs.readFileSync('./input.txt', 'utf-8');
 const lines = input.split('\r\n');
 
-const getFuel = (mass) => parseInt(mass / 3) - 2;
+const getFuel = (mass) => Math.floor(mass / 3) - 2;
 
 let iterativeSum = 0;
 lines.forEach((line) => {
