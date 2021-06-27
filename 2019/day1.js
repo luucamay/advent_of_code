21665aed149ee1baab01711a2f9383e67c4936d4
Lupe CaMay <lupemaydana@gmail.com>
2021-06-27 11:44:49 -0700

    Feat: Add recursive solution

--- 2019/day1.js
+++ 2019/day1.js
@@ -4,10 +4,21 @@ const lines = input.split('\r\n');
 
 const getFuel = (mass) => Math.floor(mass / 3) - 2;
 
+const getFuelRecursive = (mass, calculatedSum = 0) => {
+  if (mass <= 0)
+    return calculatedSum
+  const nFuel = getFuel(mass)
+  return getFuelRecursive(nFuel, calculatedSum + Math.max(0, nFuel))
+}
+
 let iterativeSum = 0;
+let recursiveSum = 0
 lines.forEach((line) => {
   const mass = Number(line);
-  iterativeSum += getFuel(mass)
+  iterativeSum += getFuel(mass);
+  recursiveSum += getFuelRecursive(mass)
 })
 
-console.log(iterativeSum);
+
+
+console.log(recursiveSum);
